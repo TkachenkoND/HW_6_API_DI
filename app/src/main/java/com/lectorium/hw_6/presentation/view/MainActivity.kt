225c9bd35ee11b3cd2ee574e4a46bcff796a0f61
Tkachenko Nikita <tkachenkond00@gmail.com>
2022-01-21 09:28:03 +0200

    created the project structure

--- app/src/main/java/com/lectorium/hw_6/presentation/view/MainActivity.kt
+++ app/src/main/java/com/lectorium/hw_6/presentation/view/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.lectorium.hw_6
+package com.lectorium.hw_6.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lectorium.hw_6.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
