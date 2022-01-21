f60ff9ca12fd80ad1a8354a4119d0ea099942e6e
Tkachenko Nikita <tkachenkond00@gmail.com>
2022-01-21 16:21:06 +0200

    created list item activity

--- app/src/main/java/com/lectorium/hw_6/presentation/view/MainActivity.kt
+++ app/src/main/java/com/lectorium/hw_6/presentation/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.lectorium.hw_6.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lectorium.hw_6.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
