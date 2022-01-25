51225b36899a72ab5e19242b61b3a36f8aee8339
Tkachenko Nikita <tkachenkond00@gmail.com>
2022-01-25 03:04:59 +0200

    add fragment in project

--- app/src/main/java/com/lectorium/hw_6/presentation/view/ListItemsActivity.kt
+++ app/src/main/java/com/lectorium/hw_6/presentation/view/ListItemsActivity.kt
@@ -2,37 +2,27 @@ package com.lectorium.hw_6.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import com.lectorium.hw_6.R
 import com.lectorium.hw_6.databinding.ListItemsActivityBinding
-import com.lectorium.hw_6.presentation.view_model.ListItemsActivityViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ListItemsActivity : AppCompatActivity() {
 
     lateinit var binding: ListItemsActivityBinding
-    private val viewModel by viewModel<ListItemsActivityViewModel>()
-    private val listItemAdapter = ListItemAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ListItemsActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.loadItemList()
+        val fragment = FragmentMain()
+        showFragment(fragment)
 
-        initAdapter()
-        initObserver()
     }
 
-    private fun initAdapter(){
-        binding.recyclerViewContainerItem.apply {
-            adapter = listItemAdapter
-        }
+    private fun showFragment(fragment: FragmentMain){
+        val frag = supportFragmentManager.beginTransaction()
+        frag.replace(R.id.mainFragment,fragment)
+        frag.commit()
     }
 
-    private fun initObserver(){
-        viewModel.itemList.observe(this, {
-            listItemAdapter.submitList(it)
-        })
-    }
 }
