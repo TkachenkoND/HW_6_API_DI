fa040760b6ec4d33bf0dc2ea6b733d9e1e3a2489
TkachenkoND <tkachenkond00@gmail.com>
2022-02-06 03:03:10 +0200

    added changes to FragmentMain and viewModel

--- app/src/main/java/com/lectorium/hw_6/presentation/view_model/ListItemsActivityViewModel.kt
+++ app/src/main/java/com/lectorium/hw_6/presentation/view_model/ListItemsActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.lectorium.hw_6.presentation.view_model
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,15 +8,28 @@ import androidx.lifecycle.viewModelScope
 import com.lectorium.hw_6.domain.models.Item
 import com.lectorium.hw_6.domain.usecase.LoadListItemUseCase
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class ListItemsActivityViewModel(private val loadListItemUseCase: LoadListItemUseCase) :
     ViewModel() {
+
     private val _itemList = MutableLiveData<List<Item>>()
     val itemList: LiveData<List<Item>> = _itemList
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
     fun loadItemList() {
         viewModelScope.launch {
-            _itemList.value = loadListItemUseCase.loadItemList()
+            try {
+                _itemList.postValue(loadListItemUseCase.loadItemList())
+                _isLoading.postValue(true)
+            } catch (e: Exception) {
+                Log.d("Load Exception", e.toString())
+                _isLoading.postValue(false)
+            }
         }
     }
+
+
 }
